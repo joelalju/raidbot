20b211823677aad37b597e6d420b6efa2114756c
Joel Alarcón <joelalju100@gmail.com>
2018-02-12 18:31:37 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,22 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+var rotation = "1-2-3";
 
-client.on("ready", () => {
-  console.log("I am ready!");
-});
-
+// Set the prefix
+let prefix = "!";
 client.on("message", (message) => {
-  if (message.content.startsWith("ping")) {
-    message.channel.send("pong!");
+  // Exit and stop if the prefix is not there or if user is a bot
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  if (message.content.startsWith(prefix + "raid")) {
+    rotation = "1-2-3";
+    message.channel.send(rotation);
+  } else
+  if (message.content.startsWith(prefix + "n")) {
+    if (rotation == "1-2-3") rotation = "2-3-1";
+    else if (rotation == "2-3-1") rotation = "3-1-2";
+    else rotation = "1-2-3";
+    message.channel.send(rotation);
   }
 });
 
